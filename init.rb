03749500af185562a4aa5b98f53e46ee9739e164
Igor Olemskoi <igor@southbridge.ru>
2016-02-09 19:40:45 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ end
 
 ActionDispatch::Callbacks.to_prepare do
   require 'sms_auth'
-  require_dependency 'redmine_sms_auth/hooks'
+  require_dependency 'redmine_2auth/hooks'
   require_dependency 'account_controller_patch'
   require_dependency 'user_patch'
 end
