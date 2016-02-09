e4f603a775963bb4bba1e058e71d81616f55f2f5
Igor Olemskoi <igor@southbridge.ru>
2016-02-09 19:42:13 +0300

    Update hooks.rb

--- lib/redmine_sms_auth/hooks.rb
+++ lib/redmine_sms_auth/hooks.rb
@@ -1,8 +1,8 @@
 module RedmineSmsAuth
 
   class Hooks < Redmine::Hook::ViewListener
-    render_on :view_users_form, partial: 'hooks/redmine_sms_auth/view_users_form'
-    render_on :view_my_account, partial: 'hooks/redmine_sms_auth/view_my_account'
+    render_on :view_users_form, partial: 'hooks/redmine_2auth/view_users_form'
+    render_on :view_my_account, partial: 'hooks/redmine_2auth/view_my_account'
   end
 
 end
