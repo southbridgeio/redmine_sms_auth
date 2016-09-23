d9dd4d82756b30273a786a6b60461950502574e4
Artur Trofimov <arturtr@gmail.com>
2016-09-23 17:27:45 +1000

    fix first migration for compatible with redmine_2fa plugin

--- db/migrate/001_add_sms_auth.rb
+++ db/migrate/001_add_sms_auth.rb
@@ -1,9 +1,12 @@
 class AddSmsAuth < ActiveRecord::Migration
   def up
-    AuthSourceSms.create name: 'SMS', :onthefly_register => false, :tls => false
+    AuthSourceSms.create name: 'SMS', onthefly_register: false, tls: false
   end
 
   def down
     AuthSource.where(type: 'AuthSourceSms').destroy_all
+    if Redmine::Plugin.installed?(:redmine_2fa)
+      Redmine2FA::AuthSourceSms.create name: 'SMS', onthefly_register: false, tls: false
+    end
   end
 end
