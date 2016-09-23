7efb7fc426cec636eba96c7d160cfaf622abe942
Artur Trofimov <arturtr@gmail.com>
2016-09-23 21:20:43 +1000

    fix first migration for migrate users to redmine_2fa SMS

--- db/migrate/001_add_sms_auth.rb
+++ db/migrate/001_add_sms_auth.rb
@@ -4,9 +4,24 @@ class AddSmsAuth < ActiveRecord::Migration
   end
 
   def down
-    AuthSource.where(type: 'AuthSourceSms').destroy_all
+
     if Redmine::Plugin.installed?(:redmine_2fa)
-      Redmine2FA::AuthSourceSms.create name: 'SMS', onthefly_register: false, tls: false
+      old_auth_source    = AuthSource.where(type: 'AuthSourceSms').first
+      old_auth_source_id = if old_auth_source
+                             old_auth_source.id
+                           else
+                             (User.all.pluck(:auth_source_id).compact.uniq - AuthSource.pluck(:id)).first
+                           end
+
+      AuthSource.where(type: 'AuthSourceSms').destroy_all
+
+      new_auth_source = Redmine2FA::AuthSourceSms.create name: 'SMS', onthefly_register: false, tls: false
+
+      if old_auth_source_id
+        User.where(auth_source_id: old_auth_source_id).update_all(auth_source_id: new_auth_source.id)
+      end
+    else
+      AuthSource.where(type: 'AuthSourceSms').destroy_all
     end
   end
 end
